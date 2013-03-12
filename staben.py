7b46d8d7f2b34f3c9fa2d7f4c9a297ee483943f6
Johan Wanglof <jwanglof@gmail.com>
2013-03-12 19:00:48 +0100

    Added all the routes for the templates

--- staben.py
+++ staben.py
@@ -5,7 +5,7 @@ from dev import db_commands
 app = config.app
 
 # DEV OPTIONS
-# NEED TO BE REMOVED IN PRODUCTION MODE
+# NEEDS TO BE REMOVED IN PRODUCTION MODE
 @app.route('/db_create')
 def db_create():
 	return db_commands.create_app()
@@ -13,6 +13,22 @@ def db_create():
 @app.route('/')
 def index():
 	return config.render_template('index.html')
+
+@app.route('/schedule')
+def schedule():
+	return config.render_template('schedule.html')
+
+@app.route('/student_poll')
+def student_poll():
+	return config.render_template('student_poll.html')
+
+@app.route('/gallery')
+def gallery():
+	return config.render_template('gallery.html')
+
+@app.route('/blog')
+def blog():
+	return config.render_template('blog.html')
 
 @app.route('/contact')
 def contact():
