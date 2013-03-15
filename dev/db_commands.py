f9919247ce75dfa4e9f8f7cd8dbcad68d4298925
Johan Wanglof <jwanglof@gmail.com>
2013-03-15 16:53:51 +0100

    Got an error while creating the DB, tried to fix it but didn't. Will check on it later

--- dev/db_commands.py
+++ dev/db_commands.py
@@ -1,4 +1,4 @@
-#import model
+import model
 import config
 
 db = config.db
@@ -6,7 +6,12 @@ app = config.app
 
 # Should check if the DB is created successfully or not!
 def create_app():
-    #db.init_app(app)
-    #with app.test_request_context():
-    db.create_all()
-    return "Done"
+	#db.init_app(app)
+	#with app.test_request_context():
+	db.create_all()
+	return "Done"
+
+def create_admin_users():
+    	db.session.add(model.waeggen_user)
+    	#db.session.add(model.simba_user)
+    	db.session.commit()
--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 # http://www.capsunlock.net/2011/05/coding-with-flask-and-sqlalchemy.html
 # Why UNIQUE=TRUE: http://stackoverflow.com/questions/707874/differences-between-index-primary-unique-fulltext-in-mysql
+# 
 
 '''
 * IMPORTANT!
@@ -35,7 +36,7 @@ class Blog(db.Model):
     __tablename__ = 'blog'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), index=True, unique=True)
-    text = db.Column(db.Text(), index=True, unique=True)
+    text = db.Column(db.Text, index=True, unique=True)
     date = db.Column(db.Date, index=True, unique=True)
     time = db.Column(db.Time, index=True, unique=True)
     author = db.Column(db.String(40), index=True, unique=True)
@@ -69,6 +70,13 @@ class BlogComments(db.Model):
     def __repr__(self):
         return 'The blog comment belongs to %i, was written by %s and contains %s' % (self.belong_to_id, self.author, self.comment)
 
+'''
+    Users will represent:
+    * STABEN (admins)                                  (role=1)
+    * Faddrar (Oeverfaddrar, Faddrar)       (role=2)
+    * Klassfoerestandare                                (role=3)
+    * Andra                                                        (role=4)
+'''
 class Users(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -76,7 +84,8 @@ class Users(db.Model):
     firstname = db.Column(db.String(100), index=True, unique=True)
     lastname = db.Column(db.String(100), index=True, unique=True)
     title = db.Column(db.String(15), index=True, unique=True)
-    phonenumber = db.Column(db.Integer, index=True, unique=True)
+    #phonenumber = db.Column(db.Integer, index=True, unique=True)
+    phonenumber = db.Column(db.String(15), index=True, unique=True)
     role = db.Column(db.SmallInteger, default=3)
 
     def __init__(self, email=None, firstname=None, lastname=None, title=None, phonenumber=None, role=3):
@@ -89,6 +98,9 @@ class Users(db.Model):
 
     def __repr__(self):
         return 'The user named %s %s has title %s' % (self.firstname, self.lastname, self.title)
+
+waeggen_user = Users('jwanglof@gmail.com', 'Johan', 'Wanglof', 'WebmastAH', '0046708601911', 1)
+simba_user = Users('simis.linden@gmail.com', 'Simon', 'Linden', 'WebmastAH', '0046735026994', 1)
 
 '''class StudentPoll(db.Model):
     __tablename__ = 'student_poll'
