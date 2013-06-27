aef9d53e460f929cef09bb71b3fb887d65fbaf1c
jwanglof <jwanglof@gmail.com>
2013-06-27 21:11:07 +0200

    Change a whole lot! Saw on the Flask website that we had done the DB connection wrong so I fixed that, seems to be running a lot faster, but with that change I had to change a lot in db_commands which I hope works as it should

--- config.py
+++ config.py
@@ -5,15 +5,23 @@ from flask import Flask, request, session, g, redirect, url_for, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt
 from sqlalchemy import Table, Column, Integer, String, Date, Time, Text, create_engine
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship, backref, scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 dbhost = '127.0.0.1'
 dbuser = 'STABEN'
 dbpass = 'generalhenrik'
 dbname = 'STABEN'
 
-SQLALCHEMY_DATABASE_URI = 'mysql://' + dbuser + ':' + dbpass + '@' + dbhost + '/' +dbname
-SQLALCHEMY_MIGRATE_REPO = 'db_repo'
+# URI is needed for migrations
+# We don't use this right now
+# SQLALCHEMY_DATABASE_URI = 'mysql://' + dbuser + ':' + dbpass + '@' + dbhost + '/' +dbname
+# SQLALCHEMY_MIGRATE_REPO = 'db_repo'
+
+engine = create_engine('mysql://' + dbuser + ':' + dbpass + '@' + dbhost + '/' +dbname, convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
 
 DEBUG = True
 SECRET_KEY = ':\xbe\xef\xc9\xbf\xf6\x86\x8d\xeb\x90\xa5!+\x97i\xa38\xe0\x98\x7f\xec\xca*\x8c'
@@ -25,7 +33,7 @@ ALLOWED_EXTENSIONS = set(['png','jpg','jpeg'])
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+# app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
