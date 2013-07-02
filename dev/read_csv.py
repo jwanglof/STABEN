09a1ba07a51b87f9b2b848c7285dc3d42064498d
Johan Wanglof <jwanglof@gmail.com>
2013-07-02 06:48:06 +0200

    Removed 2 global variables. Added get_dialects() that will get all the dialects from the CSV file (ex. BRAK, Idolbok). Added get_points() that will get the points from the CSV file

--- dev/read_csv.py
+++ dev/read_csv.py
@@ -5,10 +5,8 @@ import csv
 # since dict's aren't ordered
 ###
 class ReadStudentPollCsvFile():
-	prefixes = {}
-	questions = {}
-
 	def __init__(self, filename):
+		csv.register_dialect('commas', delimiter=',')
 		self.filename = filename
 
 	# Check so that row doesn't contain any 'illegal' characters
@@ -20,39 +18,69 @@ class ReadStudentPollCsvFile():
 
 	def get_prefixes(self):
 		i = 1
+		prefixes = {}
+
 		with open(self.filename, 'rb') as csvfile:
 			reader = csv.reader(csvfile)
 			for row in reader:
 				if self.check(row[0]) and row[0][:1].isupper():
-					self.prefixes[i] = row[0]
+					prefixes[i] = row[0]
 					i += 1
-		return self.prefixes
+		return prefixes
 
 	def get_questions(self):
 		i = 0
 		o = 0
 		tmp_list = []
+		questions = {}
 
 		with open(self.filename, 'rb') as csvfile:
 			reader = csv.reader(csvfile)
 			for row in reader:
 				if self.check(row[0]) and row[0][:1].isupper():
 					if i > 0:
-						self.questions[i] = tmp_list
+						questions[i] = tmp_list
 						tmp_list = []
 					i += 1
 
 				if self.check(row[0]) and row[0][:1].islower():
 					tmp_list.append(row[0])
 
-		# Need this so the last list is added to self.questions!
-		self.questions[i] = tmp_list
-		return self.questions
+		# Need this so the last list is added to questions!
+		questions[i] = tmp_list
+		return questions
+
+	def get_dialects(self):
+		dialects = {}
+		with open(self.filename, 'rb') as csvfile:
+			reader = csv.reader(csvfile, dialect='commas')
+			dialect_list = [x for x in reader][0][1:]
+
+		for index, x in enumerate(dialect_list):
+			dialects[index+1] = x
+		return dialects
+
+	def get_points(self):
+		point_list = []
+		points = {}
+		with open(self.filename, 'rb') as csvfile:
+			reader = csv.reader(csvfile, dialect='commas')
+			for row in reader:
+				if self.check(row[0]) and row[0][:1].islower():
+					point_list.append(row)
+
+		questions = [y for x in self.get_questions().values() for y in x]
+		for i, q in enumerate(questions):
+			# print 'Question: ' + point_list[i][0]
+			# print 'Points: ', point_list[i][1:]
+			# points[i+1] = point_list[i]
+			points[i+1] = {point_list[i][0]: point_list[i][1:]}
+		return points
+
 
 ## Run example:
 # x = ReadStudentPollCsvFile('studentpoll.csv')
-# print x.get_prefixes()
-# print x.get_questions()
-#
-## Will return:
-## 1: ['dattamaskin', 'blabla', 'bla'], 2: ['dddd', 'ddd'] etc
+# print x.get_prefixes()	## {1: 'Har med sig', 2: 'Gillar', ....}
+# print x.get_questions()	## {1: ['dattamaskin', 'blabla', 'bla'], 2: ['dddd', 'ddd'], ....}
+# print x.get_dialects()	## {1: 'Idolbok', 2: 'MacGyver', ....}
+# print x.get_points()		## {1: ['dattamaskin', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '1', '', '', '', '1', '', '', '', '', '', '', '2', ''], ....}
