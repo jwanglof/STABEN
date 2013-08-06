20a2b6304c4769b297858f2e084e641b8d9d8d58
Johan Wanglof <jwanglof@gmail.com>
2013-08-06 15:47:04 +0200

    Added check_if_email_exist() in register() and moved some shieeeeet around

--- staben.py
+++ staben.py
@@ -122,10 +122,14 @@ def login():
 				# user_info['info'] contains all the user's information (from the table userInformation)
 				if add_session(user):
 					db_commands.add_login_count(request.form['email'])
-					if session['finished_profile']:
-						redirect_to = 'profile'
-					else:
+
+					if not session['finished_profile']:
 						redirect_to = 'profile_edit'
+					elif not session['poll_done']:
+						redirect_to = 'profile_student_poll'
+					else:
+						redirect_to = 'profile'
+
 					return redirect(url_for(redirect_to, user_email=request.form['email']))
 			else:
 				return render('login.html', login=False)
@@ -142,43 +146,48 @@ def signout():
 @app.route('/register', methods=['POST', 'GET'])
 def register():
 	if request.method == 'POST':
-		forbidden_chars = [u'å', u'ä', u'ö']
-		forbidden_chars = [x for x in forbidden_chars if x in request.form['email']]
+		if not db_commands.check_if_email_exist(request.form['email']):
+			forbidden_chars = [u'å', u'ä', u'ö']
+			forbidden_chars_email = [x for x in forbidden_chars if x in request.form['email']]
 
-		if len(forbidden_chars) == 0:
-			if request.form['email'] != '' and str(request.form['regCode']) == str(db_commands.get_register_code().code):
-				debug('register', 'email och code funkar')
-				if request.form['password'] == request.form['rep_password']:
-					debug('register', 'Passwords are the same')
-					if db_commands.register_user(request.form):
-						user = db_commands.get_db_user(db_user_email=request.form['email'], db_user_password=request.form['password'])['user']
+			if len(forbidden_chars_email) == 0:
+				if request.form['email'] != '' and str(request.form['regCode']) == str(db_commands.get_register_code().code):
+					debug('register', 'email och code funkar')
+					if request.form['password'] == request.form['rep_password']:
+						debug('register', 'Passwords are the same')
+						if db_commands.register_user(request.form):
+							user = db_commands.get_db_user(db_user_email=request.form['email'], db_user_password=request.form['password'])['user']
 
-						if user:
-							if db_commands.add_user_information(user.id):
-								debug('register', 'Registration succeeded')
-								user = db_commands.get_db_user(user_id=user.id)
-								add_session(user)
-								return redirect(url_for('profile_edit', user_email=user['user'].email))
+							if user:
+								if db_commands.add_user_information(user.id):
+									debug('register', 'Registration succeeded')
+									user = db_commands.get_db_user(user_id=user.id)
+									add_session(user)
+									return redirect(url_for('profile_edit', user_email=user['user'].email))
+								else:
+									debug('register', 'Error, could not add user information')
+									return redirect(url_for('register'))
 							else:
-								debug('register', 'Error, could not add user information')
+								debug('register', 'Error, could not get user')
 								return redirect(url_for('register'))
 						else:
-							debug('register', 'Error, could not get user')
+							debug('register', 'Error, could not register user')
 							return redirect(url_for('register'))
 					else:
-						debug('register', 'Error, could not register user')
+						debug('register', 'Error, the password did not match')
+						flash(u'Du måste ange samma lösenord i båda rutorna.')
 						return redirect(url_for('register'))
 				else:
-					debug('register', 'Error, the password did not match')
-					flash(u'Du måste ange samma lösenord i båda rutorna.')
+					debug('register', 'Error, the user did not type his email and register code')
+					flash(u'Du måste ange din e-mail och registreringskod!')
 					return redirect(url_for('register'))
 			else:
-				debug('register', 'Error, the user did not type his email and register code')
-				flash(u'Du måste ange din e-mail och registreringskod!')
+				debug('register', 'Error, the user typed a forbidden character as his e-mail')
+				flash(u'Din e-post får inte innehålla å, ä eller ö!')
 				return redirect(url_for('register'))
 		else:
-			debug('register', 'Error, the user typed a forbidden character as his e-mail')
-			flash(u'Din e-post får inte innehålla å, ä eller ö!')
+			debug('register', 'Error, the email already exists')
+			flash(u'E-posten du angav är redan registrerad!')
 			return redirect(url_for('register'))
 	else:
 		return render('register.html')
@@ -301,14 +310,14 @@ def profile_save_student_poll(user_email):
 @app.route('/admin/pages/')
 def admin_pages():
 	# Need to check that the user is signed in and is an admin
-	if db_commands.admin_check(session['email']) == 0:
+	if db_commands.check_role(session['email']) == 0:
 		return render('admin_pages.html')
 	else:
 		return render('admin_fail.html')
 
 @app.route('/admin/addcontact/', methods=['GET', 'POST'])
 def admin_addcontact():
-	if db_commands.admin_check(session['email']) is 0:
+	if db_commands.check_role(session['email']) is 0:
 		if request.method == 'POST':
 			result = db_commands.add_contact(request.form['name'], request.form['phonenumber'],
 											 request.form['email'],request.form['role'],
@@ -322,7 +331,7 @@ def admin_addcontact():
 @app.route('/admin/users/')
 def admin_get_all_users():
 	# Need to check that the user is signed in and is an admin
-	if db_commands.admin_check(session['email']) is 0:
+	if db_commands.check_role(session['email']) is 0:
 		users = db_commands.admin_get_all_users()
 		return render('admin_get_all_users.html', users=users)
 	else:
@@ -330,7 +339,7 @@ def admin_get_all_users():
 
 @app.route('/admin/student_poll/')
 def admin_student_poll():
-	if db_commands.admin_check(session['email']) is 0:
+	if db_commands.check_role(session['email']) is 0:
 		return render('admin_student_poll.html', \
 			prefixes=db_commands.get_student_poll_prefix(), \
 			dialects=db_commands.get_student_poll_dialects())
@@ -359,7 +368,7 @@ def admin_student_poll_save(command):
 
 @app.route('/admin_student_poll_result')
 def admin_student_poll_result():
-	if db_commands.admin_check(session['email']) is 0:
+	if db_commands.check_role(session['email']) is 0:
 		# print db_commands.admin_get_top_three_groups()[1]['user'].firstname
 		return render('admin_student_poll_result.html', \
 			users_info=db_commands.admin_get_all_users_w_poll_done(), \
@@ -370,7 +379,7 @@ def admin_student_poll_result():
 
 @app.route('/admin_show_student_poll_result/<user_id>')
 def admin_show_student_poll_result(user_id):
-	if db_commands.admin_check(session['email']) is 0:
+	if db_commands.check_role(session['email']) is 0:
 		alot_of_info = db_commands.admin_get_user_poll_answer(user_id)
 		return render('admin_student_poll_user_result.html', \
 			user=alot_of_info[1], \
