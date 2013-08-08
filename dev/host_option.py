5743ed6deac03eda66b62b108c71fedaf5ab097c
Johan Wanglof <jwanglof@gmail.com>
2013-08-08 21:21:53 +0200

    Added a url for studentpoll.csv

--- dev/host_option.py
+++ dev/host_option.py
@@ -1,10 +1,14 @@
+import os
+
 local = True
 
 if local:
 	dbuser = 'STABEN'
 	dbpass = 'generalhenrik'
 	dbname = 'STABEN'
+	student_poll_file = os.getcwd() + '/dev/studentpoll.csv'
 else:
 	dbuser = 'dstaben'
 	dbpass = 'cjnW5A82YhcBWAcK'
 	dbname = 'dstaben'
+	student_poll_file = '/www/dstaben/htdocs/dev/studentpoll.csv'
