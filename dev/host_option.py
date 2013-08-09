0abc1e5359ea7d3b6aae91b880b95530c27efd12
Johan Wanglof <jwanglof@gmail.com>
2013-08-09 13:33:29 +0200

    Fixed paths

--- dev/host_option.py
+++ dev/host_option.py
@@ -6,9 +6,9 @@ if local:
 	dbuser = 'STABEN'
 	dbpass = 'generalhenrik'
 	dbname = 'STABEN'
-	student_poll_file = os.getcwd() + '/dev/studentpoll.csv'
+	student_poll_file = os.getcwd() + '/dev/files/studentpoll.csv'
 else:
 	dbuser = 'dstaben'
 	dbpass = 'cjnW5A82YhcBWAcK'
 	dbname = 'dstaben'
-	student_poll_file = '/www/dstaben/htdocs/dev/studentpoll.csv'
+	student_poll_file = '/www/dstaben/htdocs/dev/files/studentpoll.csv'
