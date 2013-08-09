cb9a42259db1e534085496af39efd77ed6def0ff
Johan Wanglof <jwanglof@gmail.com>
2013-08-09 14:11:50 +0200

    Added quote_file

--- dev/host_option.py
+++ dev/host_option.py
@@ -7,8 +7,10 @@ if local:
 	dbpass = 'generalhenrik'
 	dbname = 'STABEN'
 	student_poll_file = os.getcwd() + '/dev/files/studentpoll.csv'
+	quote_file = os.getcwd() + '/dev/files/quotes.txt'
 else:
 	dbuser = 'dstaben'
 	dbpass = 'cjnW5A82YhcBWAcK'
 	dbname = 'dstaben'
 	student_poll_file = '/www/dstaben/htdocs/dev/files/studentpoll.csv'
+	quote_file = '/www/dstaben/htdocs/dev/files/quotes.txt'
