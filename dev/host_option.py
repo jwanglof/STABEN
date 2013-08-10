63402f656cd2e3c10d9dc98055685a6f6ad52c5d
Johan Wanglof <jwanglof@gmail.com>
2013-08-10 11:46:08 +0200

    Added dbhost

--- dev/host_option.py
+++ dev/host_option.py
@@ -3,12 +3,14 @@ import os
 local = True
 
 if local:
+	dbhost = '127.0.0.1'
 	dbuser = 'STABEN'
 	dbpass = 'generalhenrik'
 	dbname = 'STABEN'
 	student_poll_file = os.getcwd() + '/dev/files/studentpoll.csv'
 	quote_file = os.getcwd() + '/dev/files/quotes.txt'
 else:
+	dbhost = '127.0.0.1'
 	dbuser = 'dstaben'
 	dbpass = 'cjnW5A82YhcBWAcK'
 	dbname = 'dstaben'
