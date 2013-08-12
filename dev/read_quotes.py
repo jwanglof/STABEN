4f5d6e891f9ab692185dd01c70b1645ec4f25571
Johan Wanglof <jwanglof@gmail.com>
2013-08-12 11:04:59 +0200

    Fixed check_string. Not really fancy so it should be re-done

--- dev/read_quotes.py
+++ dev/read_quotes.py
@@ -1,17 +1,33 @@
+import re
+
 class ReadQuotes():
 	def __init__(self, filename):
 		self.file = open(filename, 'rb').read().splitlines()
 
+	def strip_string(self, string):
+		return string.replace('.', '').replace('"', '')
+
 	def check_string(self, string):
-		check_for = ['staben', 'generalen']
-		for x in string.lower().split():
-			print [string.lower().find(y) for y in check_for]
-				# for y in check_for:
-				# 	pos = string.lower().find(y)
-				# 	length = len(y)
-					# print string[pos:length]
-					# print string
-		# print string.lower().find([a for a in check_for])
+		# Replace with a dict
+		# {'staben': '<font staben>'}
+		check_for = ['staben', 'generalen', 'nollan', 'minus']
+
+		# Use reg ex instead
+		# E.g. periods are removed if it says i.e. '... generalen. Aasd'
+		string = string.split()
+		for word in string:
+			stripped_word = self.strip_string(word).lower()
+			if stripped_word in check_for:
+				list_index = string.index(word)
+				if stripped_word == 'staben':
+					string[list_index] = '<span class=\'stabenfont\'>STABEN</span>'
+				elif stripped_word == 'generalen':
+					string[list_index] = '<span class=\'stabenfont\'>GENERALEN</span>'
+				elif stripped_word == 'nollan':
+					string[list_index] = '<span class=\'nollanfont\'>nollan</span>'
+				elif stripped_word == 'minus':
+					string[list_index] = '<span class=\'nollanfont\'>nollan</span>'	
+		return ' '.join(string)
 
 	def get_quotes(self):
 		quote_list = []
@@ -22,22 +38,10 @@ class ReadQuotes():
 			elif line[-2] is '.':
 				line = line[:-2]
 
-			# Send string to check_string() instead. FIIIIX!
-			quote_list.append(line.replace('"', '').\
-				replace('STABEN', '<span class=\'stabenfont\'>STABEN</span>').\
-				replace('staben', '<span class=\'stabenfont\'>STABEN</span>').\
-				replace('Staben', '<span class=\'stabenfont\'>STABEN</span>').\
-				replace('GENERALEN', '<span class=\'stabenfont\'>GENERALEN</span>').\
-				replace('generalen', '<span class=\'stabenfont\'>GENERALEN</span>').\
-				replace('Generalen', '<span class=\'stabenfont\'>GENERALEN</span>').\
-				replace('Nollan', '<span class="nollanfont">Nollan</span>').\
-				replace('nollan', '<span class="nollanfont">nollan</span>').\
-				replace('Nollans', '<span class="nollanfont">Nollans</span>').\
-				replace('nollans', '<span class="nollanfont">nollans</span>').\
-				replace('Minus', '<span class="nollanfont">Nollan</span>').\
-				replace('minus', '<span class="nollanfont">nollan</span>'))
+			quote_list.append(self.check_string(line))
 		return quote_list
 
 ## Run example:
 # x = ReadQuotes('files/quotes.txt')
 # print x.get_quotes()			## ['Fru och barn', 'STABEN kan ro utan vatten', ...]
+# print x.check_string('STABEN ser allt, Nollan. Generalen har mer skagg!')
