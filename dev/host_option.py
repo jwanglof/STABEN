af786c22c1cd1cf7f95dc99e0ad23a4ce8a53f18
Johan Wanglof <jwanglof@gmail.com>
2013-08-23 10:42:05 +0200

    Added HOST

--- dev/host_option.py
+++ dev/host_option.py
@@ -4,12 +4,13 @@ local = True
 dev = True
 
 if local:
-	dbhost = '0.0.0.0'
+	dbhost = '127.0.0.1'
 	dbuser = 'STABEN'
 	dbpass = 'generalhenrik'
 	dbname = 'STABEN'
 	root_path = os.getcwd()
 	DEBUG = True
+	HOST = '127.0.0.1'
 else:
 	dbhost = '127.0.0.1'
 	dbuser = 'dstaben'
@@ -17,6 +18,7 @@ else:
 	dbname = 'dstaben'
 	root_path = '/www/dstaben/htdocs'
 	DEBUG = False
+	HOST = '0.0.0.0'
 
 student_poll_file = root_path + '/dev/files/studentpoll.csv'
 quote_file = root_path + '/dev/files/quotes.txt'
