1af0e435da11fef3ccdbc63646d074fc30db24e8
Johan Wanglof <jwanglof@gmail.com>
2014-01-18 20:40:19 +0100

    Comments

--- dev/read_csv.py
+++ dev/read_csv.py
@@ -1,29 +1,60 @@
 import csv
 
-###
-# Need to add the questions to a list
-# since dict's aren't ordered
-###
-
-###
-# Run example:
-# - FIRST!
-# -- Make sure that dev/studentpoll.csv is up to date
+## Read student poll csv file
+#
+# Reads a CSV file with all the prefixes, questions, dialects and max students for the student poll
+#
+# Run example
+# - Make sure that dev/studentpoll.csv is up to date!
+#
+# - studentPollCsv = ReadStudentPollCsvFile('studentpoll.csv')
+# - print studentPollCsv.get_prefixes()
+#  + {1: 'Har med sig', 2: 'Gillar', ....}
+# - print studentPollCsv.get_questions()
+#  + {1: ['dattamaskin', 'blabla', 'bla'], 2: ['dddd', 'ddd'], ....}
+# - print studentPollCsv.get_dialects()
+#  + {1: 'Idolbok', 2: 'MacGyver', ....}
+# - print studentPollCsv.get_points()
+#  + {1: ['dattamaskin', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '1', '', '', '', '1', '', '', '', '', '', '', '2', ''], ....}
+# - print studentPollCsv.get_max_students()
+#  + {1: '8', 2: '8', ....}
 # 
 # After that just run /db_all (or just /db_student_poll for just the student poll DB entries)
-###
+# 
+# @author 	Johan Wänglöf
+# @date	2013-07 - 2013-08
+# @version 	1
 class ReadStudentPollCsvFile():
+	## The constructor
+	# 
+	# @param filename Set from which file to read from
 	def __init__(self, filename):
 		csv.register_dialect('commas', delimiter=',')
 		self.filename = filename
 
+	## Check for illegal chars
+	# 
+	# @param row Specifies which row that needs to be checked
+	# 
 	# Check so that row doesn't contain any 'illegal' characters
+	# Just add the illegal chars from the document on the row
+	#
+	# This can probably be more optimized!
 	def check(self, row):
 		if row[:1] == ',' or row == 'SUmma' or row == 'Summa' or row == '' or row == 'Summa':
 			return False
 		else:
 			return True
 
+	## Get prefixes
+	# 
+	# Get all the prefixes from the file
+	# 
+	# Examples:
+	# Har med sig, Gillar, Bekväm med etc
+	# 
+	# Rules:
+	# The prefix MUST begin with an uppercase. This is needed so the script can see if it is a prefix or a question
 	def get_prefixes(self):
 		i = 1
 		prefixes = {}
@@ -36,6 +67,12 @@ class ReadStudentPollCsvFile():
 					i += 1
 		return prefixes
 
+	## Get questions
+	# 
+	# Get all questions from the file
+	# 
+	# Examples:
+	# dattamaskin, verktygslåda, målargrejor etc
 	def get_questions(self):
 		# i will determine which dialect id the question belongs to
 		# question_list contains all the questions
@@ -49,11 +86,11 @@ class ReadStudentPollCsvFile():
 			for row in reader:
 				if self.check(row[0]) and row[0][:1].isupper():
 					if i > 0:
-						print question_list
 						questions[i] = question_list
 						question_list = []
 					i += 1
 
+				# Replace each staben with the correct font
 				if self.check(row[0]) and row[0][:1].islower():
 					question_list.append(row[0].replace('staben', '<span class=\'stabenfont\'>STABEN</span>'))
 
@@ -61,6 +98,13 @@ class ReadStudentPollCsvFile():
 		questions[i] = question_list
 		return questions
 
+	## Get dialects
+	# 
+	# Get all the dialects from the file
+	# Dialects are the different groups
+	# 
+	# Examples:
+	# Idolbok,MacGyver,Mission Impossible
 	def get_dialects(self):
 		dialects = {}
 		with open(self.filename, 'rb') as csvfile:
@@ -71,6 +115,10 @@ class ReadStudentPollCsvFile():
 			dialects[index+1] = x
 		return dialects
 
+	## Get points
+	# 
+	# Get the points for each question
+	# This can probably be re-written to be more optimized!
 	def get_points(self):
 		point_list = []
 		points = {}
@@ -88,24 +136,23 @@ class ReadStudentPollCsvFile():
 			points[i+1] = {point_list[i][0]: point_list[i][1:]}
 		return points
 
+	## Get max students
+	# 
+	# Get the maximum amount of students that are allowed to be in a group
+	# Is placed at the bottom of the file
+	#
+	# Rules:
+	# The line needs to start with max_students (or something similar) because this text is removed
 	def get_max_students(self):
 		max_students_w_dialect = {}
 
 		with open(self.filename, 'rb') as csvfile:
+			## [-1] = Take the last column in the file
+			## [1:] = Remove the first element (which in this case is max_students)
 			reader = csv.reader(csvfile, dialect='commas')
-			# [-1] = Take the last column in the file
-			# [1:] = Remove the first element (which in this case is max_students)
 			max_student_list = [y for y in reader][-1][1:]
 
 		for d_id, dialect in (self.get_dialects()).iteritems():
 			max_students_w_dialect[d_id] = max_student_list[d_id-1]
 
 		return max_students_w_dialect
-			
-## Run example:
-# x = ReadStudentPollCsvFile('studentpoll.csv')
-# print x.get_prefixes()		## {1: 'Har med sig', 2: 'Gillar', ....}
-# print x.get_questions()		## {1: ['dattamaskin', 'blabla', 'bla'], 2: ['dddd', 'ddd'], ....}
-# print x.get_dialects()		## {1: 'Idolbok', 2: 'MacGyver', ....}
-# print x.get_points()			## {1: ['dattamaskin', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '1', '', '', '', '1', '', '', '', '', '', '', '2', ''], ....}
-# print x.get_max_students()	## {1: '8', 2: '8', ....}
--- dev/read_quotes.py
+++ dev/read_quotes.py
@@ -1,12 +1,39 @@
 import re
 
+## Read quotes
+# 
+# Reads quotes from a filename
+#
+# Run example:
+# - quotesRead = ReadQuotes('files/quotes.txt')
+# - print quotesRead.get_quotes()
+#  + ['Fru och barn', 'STABEN kan ro utan vatten', ...]
+# - print quotesRead.check_string('STABEN ser allt, Nollan. Generalen har mer skagg!')
+#  + '<span class=\'stabenfont\'>STABEN</span> ser allt, <span class=\'nollanfont\'>nollan</span>.  <span class=\'stabenfont\'>GENERALEN</span> har mer skagg!'
+#
+# @author 	Johan Wänglöf
+# @date	2013-08
+# @version 	1
 class ReadQuotes():
+	## The constructor
+	# 
+	# @param filename Set from which file to read from
 	def __init__(self, filename):
 		self.file = open(filename, 'rb').read().splitlines()
 
+	## Strip string
+	# 
+	# @param string Specifies which string to strip the illegal characters from
+	# 
+	# Strip string from illegal characters
 	def strip_string(self, string):
 		return string.replace('.', '').replace('"', '')
 
+	## Check string
+	# 
+	# @param string Specifies which string to check
+	#
+	# Check a string for special words and replace them with HTML code so they are shown correctly
 	def check_string(self, string):
 		# Replace with a dict
 		# {'staben': '<font staben>'}
@@ -29,8 +56,12 @@ class ReadQuotes():
 					string[list_index] = '<span class=\'nollanfont\'>nollan</span>'	
 		return ' '.join(string)
 
+	## Get quotes
+	# 
+	# Get the quotes from the file
 	def get_quotes(self):
 		quote_list = []
+
 		# Remove the last period
 		for line in self.file:
 			if line[-1] is '.':
@@ -40,8 +71,3 @@ class ReadQuotes():
 
 			quote_list.append(self.check_string(line))
 		return quote_list
-
-## Run example:
-# x = ReadQuotes('files/quotes.txt')
-# print x.get_quotes()			## ['Fru och barn', 'STABEN kan ro utan vatten', ...]
-# print x.check_string('STABEN ser allt, Nollan. Generalen har mer skagg!')
